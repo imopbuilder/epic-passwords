ab8b78754e495514c2393fefc9f91c63ee03ecbe
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2024-01-01 19:27:45 +0530

    feat: display the length value for the length slider

--- src/components/pages/home/client.tsx
+++ src/components/pages/home/client.tsx
@@ -26,6 +26,7 @@ function GeneratePasswordForm() {
 	const [uppercase, setUppercase] = useState(true);
 	const [digits, setDigits] = useState(true);
 	const [specialCharacters, setSpecialCharacters] = useState(true);
+	const [length, setLength] = useState(8);
 
 	function handleGeneratePassword() {
 		console.log(lowercase, uppercase, digits, specialCharacters);
@@ -43,26 +44,40 @@ function GeneratePasswordForm() {
 		<div>
 			<p className='p-2 px-3 border rounded-md mb-3 text-muted-foreground text-sm h-11 flex items-center justify-start'>{password}</p>
 			<div className='space-y-3'>
-				<div className='pb-2'>
+				<div className='pb-2 pt-1'>
 					<Progress className='w-full' value={progress} />
 				</div>
-				<div className='flex items-center justify-between border p-3 rounded-sm'>
+				<div className='flex items-center justify-between border p-3 rounded-md'>
 					<Label htmlFor='lowercase-letters'>Lowercase Letters</Label>
 					<Switch id='lowercase-letters' checked={lowercase} onCheckedChange={setlowercase} />
 				</div>
-				<div className='flex items-center justify-between border p-3 rounded-sm'>
+				<div className='flex items-center justify-between border p-3 rounded-md'>
 					<Label htmlFor='uppercase-letters'>Uppercase Letters</Label>
 					<Switch id='uppercase-letters' checked={uppercase} onCheckedChange={setUppercase} />
 				</div>
-				<div className='flex items-center justify-between border p-3 rounded-sm'>
+				<div className='flex items-center justify-between border p-3 rounded-md'>
 					<Label htmlFor='digits'>Digits</Label>
 					<Switch id='digits' checked={digits} onCheckedChange={setDigits} />
 				</div>
-				<div className='flex items-center justify-between border p-3 rounded-sm'>
+				<div className='flex items-center justify-between border p-3 rounded-md'>
 					<Label htmlFor='special-characters'>Special Characters</Label>
 					<Switch id='special-characters' checked={specialCharacters} onCheckedChange={setSpecialCharacters} />
 				</div>
-				<Slider className='py-2' defaultValue={[50]} max={100} step={1} />
+				<div className='border p-3 rounded-md'>
+					<div className='flex items-center justify-between pb-1'>
+						<Label>Length</Label>
+						<span className='text-sm font-bold'>{length}</span>
+					</div>
+					<Slider
+						id='length'
+						className='py-2'
+						value={[length]}
+						defaultValue={[length]}
+						max={50}
+						step={1}
+						onValueChange={(val) => setLength(Math.max(val[0], 8))}
+					/>
+				</div>
 				<Button className='w-full group' size='lg' onClick={handleGeneratePassword}>
 					Generate Password
 					<MoveRight
