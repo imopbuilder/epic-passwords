51aa9a4b423765bc670cca4a41dc863f4b6c21dc
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2024-01-01 19:03:32 +0530

    styles: change the generator align and max-width and add margin in mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function page() {
 			<Header />
 			<main>
 				<section>
-					<div className='min-h-hvh max-w-maxi mx-auto flex items-end md:items-start justify-center'>
-						<div className='py-16 w-full max-w-96'>
+					<div className='min-h-hvh max-w-maxi mx-auto flex items-start justify-center'>
+						<div className='py-16 w-full mx-[4%] sm:mx-0 sm:max-w-96'>
 							<HomeClient />
 						</div>
 					</div>
