b40f846e5601583e3a6c1cc1f754d81dc72bbf0c
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2024-01-02 08:49:31 +0530

    enhance: add constant for MAX_PASSWORD_STRENGTH in app.ts and lint progress component

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -2,5 +2,7 @@ export const CHARS = {
 	lowercase: 'abcdefghijklmnopqrstuvwxyz'.split(''),
 	uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
 	digits: '0123456789'.split(''),
-	specialCharacters: '!£$%^&*()@~:;,./?{}=-_'.split(''),
+	specialCharacters: '!£@#$%^&*()-=_+[]{}|:;"<>,.?/~`'.split(''),
 };
+
+export const MAX_PASSWORD_STRENGTH = 5;
